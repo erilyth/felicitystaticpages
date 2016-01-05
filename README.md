79b23b02cff9e014644b4f439f9a62276928648b
erilyth <vishal.batchu@students.iiit.ac.in>
2016-01-06 00:32:06 +0530

    Update styles and add animations with @varshit97, @harry-7

---
+++ README.md
@@ -0,0 +1,3 @@
+# felicitystaticpages
+Static pages for felicity
+This repo contains work of some people (:p) for making static content for felicity (our college fest) website 
---
+++ schedule/schedule.js
@@ -0,0 +1,27 @@
+var rectelem1 = document.getElementById('calendar1');
+
+var rectelem2 = document.getElementById('calendar2');
+
+function test() {
+    var rect1 = rectelem1.getBoundingClientRect();
+    var rect2 = rectelem2.getBoundingClientRect();
+    var overlap = (rect1.bottom - rect2.top) > 10;
+
+    if (overlap == true) {
+        rectelem2.style.visibility="hidden";
+    }
+    else if (overlap == false) {
+        rectelem2.style.visibility="visible";
+    }
+}
+
+setInterval(test,100);
+
+function hoverDate(text,element) {
+    element.innerHTML = text;
+    element.style.padding = 0;
+}
+
+function hoverOutDate(text,element) {
+    element.innerHTML = text;
+}
