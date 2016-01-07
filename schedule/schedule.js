2a37d14275e86c09ea39027ab6dac7516d758e48
erilyth <vishal.batchu@students.iiit.ac.in>
2016-01-07 13:45:17 +0530

    Fix overlap of calendar and timeline

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -1,16 +1,25 @@
 var rectelem1 = document.getElementById('calendar1');
-
 var rectelem2 = document.getElementById('calendar2');
+var timelineelem = document.getElementsByClassName('timeline');
 
 function test() {
     var rect1 = rectelem1.getBoundingClientRect();
     var rect2 = rectelem2.getBoundingClientRect();
+    var timelinerect = timelineelem[0].getBoundingClientRect();
+    
     var overlap = (rect1.bottom - rect2.top) > 10;
+    var overlap2 = (rect1.right - timelinerect.left) > 10;
 
     if (overlap == true) {
         rectelem2.style.visibility = "hidden";
     } else if (overlap == false) {
         rectelem2.style.visibility = "visible";
+    }
+    
+    if (overlap2 == true) {
+        rectelem1.style.visibility = "hidden";
+    } else if (overlap2 == false) {
+        rectelem1.style.visibility = "visible";
     }
 }
 
